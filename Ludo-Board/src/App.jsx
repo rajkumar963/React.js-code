70e57019609c668f18005b9d066946ea9789e552
rajkumar963 <rajkumarsaw597@gmail.com>
2024-04-23 12:16:33 +0530

    update

--- Ludo-Board/src/App.jsx
+++ Ludo-Board/src/App.jsx
@@ -2,12 +2,18 @@
 import './App.css'
 import LudoBoard from './LudoBoard.jsx'
 import TodoList from './TodoList.jsx'
+import Lottery from './Lottery.jsx'
+import TicketNum from './TicketNum.jsx'
 
 function App() {
   return ( 
     <>
         {/* <LudoBoard/> */}
-        <TodoList/>
+        {/* <TodoList/> */}
+        {/* <Lottery/> */}
+        <TicketNum num={5} />
+        <TicketNum num={4} />
+        <TicketNum num={1} />
     </>
   )
 
