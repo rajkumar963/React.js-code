861d1cab81804368ff28059be67f4647702bc851
rajkumar963 <rajkumarsaw597@gmail.com>
2025-10-23 13:29:56 +0530

    Implements feature section

--- Apple-Web/src/App.jsx
+++ Apple-Web/src/App.jsx
@@ -5,6 +5,9 @@ import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import Showcase from "./components/Showcase"
 import Footer from "./components/Footer"
+import Performance from "./components/Performance"
+import Features from "./components/Features"
+import Highlights from "./components/Highlights"
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -16,6 +19,9 @@ function App() {
        <Hero/>
        <ProductViewer/>
        <Showcase/>
+       <Performance/>
+       <Features/>
+       <Highlights/>
        <Footer/>
      </main>
   )
